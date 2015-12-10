8c863c8c14a3144ce6bbf86a5ead8c384e62d02b
Richard Kalehoff <richardkalehoff@gmail.com>
2015-12-10 11:24:57 -0800

    add literal vs constructor example

---
+++ lesson2/in-class-code/app.js
@@ -0,0 +1,8 @@
+var obj1 = {};               // A new object via Object literal
+var arr1 = [];               // A new array via Array literal
+var func1 = function() {};   // A new function via Function literal
+
+
+var obj2 = new Object();     // A new object via Object constructor
+var arr2 = new Array();      // A new array via Array constructor
+var func2 = new Function();  // A new Function via Function Constructor
