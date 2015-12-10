689ce7e681e7e0e6356142b36c14b00fe31cb040
Richard Kalehoff <richardkalehoff@gmail.com>
2015-12-10 13:22:06 -0800

    create `constructor` function

--- lesson2/in-class-code/app.js
+++ lesson2/in-class-code/app.js
@@ -28,3 +28,11 @@ var isFun = make('fun');
 isFun('biking');      // 'Biking is fun!'
 isFun('climbing');    // 'Climbing is fun!'
 isFun('broccoli');    // 'Broccoli is fun!'
+
+var numLetters = function(letter) {
+    return new Function('times', "if (times < 0) return ''; var result = ''; times = Math.round(times); while(times--) { result += '" + letter + "'; } return result;");
+};
+
+var LotsOfAaas = numLetters('a');
+
+LotsOfAaas(4.3); // 'aaaa'
