7dd827bc93574af55f138591baa1c3e011c5409f
Richard Kalehoff <richardkalehoff@gmail.com>
2015-12-10 11:28:40 -0800

    example function in literal and constructor format

--- lesson2/in-class-code/app.js
+++ lesson2/in-class-code/app.js
@@ -6,3 +6,14 @@ var func1 = function() {};   // A new function via Function literal
 var obj2 = new Object();     // A new object via Object constructor
 var arr2 = new Array();      // A new array via Array constructor
 var func2 = new Function();  // A new Function via Function Constructor
+
+
+
+var adder1 = function(num1, num2) {
+    return num1 + num2;
+};
+
+var adder2 = new Function("num1", "num2", "return num1 + num2");
+
+adder1(1, 2);
+adder2(3, 4);
