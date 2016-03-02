44caeea3aebc1475a86f09eb5aff5e13776d58bc
Richard Kalehoff <richardkalehoff@gmail.com>
2016-03-02 13:39:30 -0800

    add starter comments for Build an Event System quiz

--- lesson2/quizBuildAnEventSystem/start/app.js
+++ lesson2/quizBuildAnEventSystem/start/app.js
@@ -1,3 +1,26 @@
+// Create the your own Event Tracker system:
+//
+// 1. create an `EventTracker` object
+//    • it should accept a name when constructed
+// 2. extend the `EventTracker` prototype with:
+//    • an `on` method
+//    • a `notify` method
+//    • a `trigger` method
+//
+// EXAMPLE:
+// function purchase(item) { console.log( 'purchasing ' + item); }
+// function celebrate() { console.log( this.name + ' says birthday parties are awesome!' ); }
+//
+// var nephewParties = new EventTracker( 'nephews ');
+// var richard = new EventTracker( 'Richard' );
+//
+// nephewParties.on( 'mainEvent', purchase );
+// richard.on( 'mainEvent', celebrate );
+// nephewParties.notify( richard, 'mainEvent' );
+//
+// nephewParties.trigger( 'mainEvent', 'ice cream' );
+//
+
 var EventTracker = function(name) {
     this.name = name;
     this._events = {};
