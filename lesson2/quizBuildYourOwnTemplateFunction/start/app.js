85004c3a088c308ec5a86fd8408fc1ab44e3e3ea
Richard Kalehoff <richardkalehoff@gmail.com>
2016-03-03 16:35:21 -0800

    add starter comments for Build Your Own Template Function quiz

--- lesson2/quizBuildYourOwnTemplateFunction/start/app.js
+++ lesson2/quizBuildYourOwnTemplateFunction/start/app.js
@@ -1,3 +1,30 @@
+// Create the your own `template` function:
+//
+// • the `template` function should accept
+//    1. a string of the template to parse
+//    2. an `options` object for custom delimiters
+//        - an `open` property for the open delimiter
+//        - a `close` property for the close delimiter
+// • the default delimiters the `template` function should use are:
+//    1. `*(` for the opening delimiter
+//    2. `)*` for the closing delimiter
+// • the `template` function should return a function
+// • the returned function should accept:
+//    1. one argument for each placeholder in the original string
+//    2. a number - this is how many times the string should be logged to the console
+//
+// EXAMPLE:
+// in the example below `*(` is my default opening delimiter and `)*` is the default closing delimiter
+// var string = "Hi, my name is Richard. And I *( emotion )* this *( thing )*!";
+// var logResult = template( string );
+// logResult( 'love', 'ice cream', 2 ); // logs the message "Hi, my name is Richard. And I love this ice cream!", twice
+//
+//
+// var string = "Is <<! thing !>> healthy to <<! action !>>?";
+// var logResult = template( string, {open: '<<!', close: '!>>'} );
+// logResult( 'ice cream', 'consume', 7 ); // logs the message "Is ice cream healthy to consume?", seven times
+//
+
 var template = function(text, options) {
     var delimiter = {
         open: '*(',
